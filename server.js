cb83935afceaa8f2baf89fcdc04a3688a1c45e15
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2023-01-20 12:18:25 +0530

    create server

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+
+const cors = require("cors")
+const express = require("express")
+
+const port = process.env.port || 8081;
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log("Server is up!");
+  process.on("unhandledRejection", (ex) => {
+    throw ex;
+  });
+});
+
+
+
+
+
