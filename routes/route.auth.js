56d126da0bc40bed035aec94c00e606035d99b5c
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2023-01-20 15:47:27 +0530

    validate /logIn with Joi

--- routes/route.auth.js
+++ routes/route.auth.js
@@ -1,10 +1,10 @@
 const { signUp, logIn } = require("../controllers/controller.auth");
 const { validateWithJoi } = require("../middlewares/middleware.validations");
-const { schemaSignUp } = require("../modules/joi/joi.schemas");
+const { schemaSignUp, schemaLogIn } = require("../modules/joi/joi.schemas");
 
 const router = require("express").Router();
 
 router.post("/signUp", validateWithJoi(schemaSignUp), signUp);
-router.get("/logIn", logIn)
+router.post("/logIn", validateWithJoi(schemaLogIn), logIn)
 
 module.exports = router;
