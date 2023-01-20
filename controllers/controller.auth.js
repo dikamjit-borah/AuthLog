b88b3df2ddb1af445cc77128a9bd262960398018
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2023-01-20 14:08:25 +0530

    check user exists before creating new user

--- controllers/controller.auth.js
+++ controllers/controller.auth.js
@@ -1,16 +1,23 @@
 const serviceAuth = require("../services/service.auth")
+const { sendError, sendResponse } = require("../utils/response.handler")
 
 module.exports = {
     signUp: async function (req, res) {
         try {
             const details = { name, email, password, age, address } = { ...req.body }
+
+            const getUser = await serviceAuth.getUser(email)
+            if (getUser) return sendResponse(res, 400, "User already exists")
+
             const saveUser = await serviceAuth.saveUser(details)
-            if (saveUser) res.send("sign up success")
+            if (saveUser) return sendResponse(res, 201, "Sign up successful!", { user_id: saveUser._id })
+
+            sendError(res, null, "Sign up failed!")
+
 
         } catch (error) {
             console.log(error);
-            res.send("sign up failed")
-
+            sendError(res, error)
         }
     },
 
--- services/service.auth.js
+++ services/service.auth.js
@@ -1,6 +1,13 @@
 const { User } = require("../schemas/schema.user")
 
 module.exports = {
+    getUser: async function (email) {
+        try {
+            return await User.findOne({ email })
+        } catch (error) {
+            throw new Error(error.message)
+        }
+    },
     saveUser: async function (details) {
         try {
             return await new User(details).save()
